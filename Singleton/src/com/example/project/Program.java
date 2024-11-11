834314a2c8f83d050f8e7cb0d09cb821c20fa95d
Mukesh <mukeshlodha678@gmail.com>
2024-11-11 23:58:25 +0530

    added builder and factory design pattern

--- Singleton/src/com/example/project/Program.java
+++ Singleton/src/com/example/project/Program.java
@@ -1,15 +1,2 @@
-public class Program {
-    public static void main(String[] args) {
-       //The singleton pattern is a software design pattern that restricts the instantiation of a class to one object.
-        // This is useful when exactly one object is needed to coordinate actions across the system.
-
-        Database dbInstance =  Database.getInstance();
-        dbInstance.DBconnection();
-
-        Logger loggerInstance = Logger.getInstance();
-        loggerInstance.LoggerConnection();
-
-        ConfigurationManager CMinstance = ConfigurationManager.getInstance();
-        CMinstance.CMConnection();
-    }
+package com.example.project;public class Program {
 }
